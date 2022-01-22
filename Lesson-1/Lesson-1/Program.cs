eb47a79c7648b277c10db32620f129d416454953
efess29 <efess29@gmail.com>
2022-01-22 19:32:24 +0300

    finished
    finished

---
+++ Lesson-1/Lesson-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+
+            // получаем имя пользователя из ввода в консоли
+            string userName = Convert.ToString(Console.ReadLine());
+
+            // получаем значение текущей даты
+            DateTime currenDate = DateTime.Now;
+
+            // выводим имя пользователя и текущую дату в коротком строковом представлении
+            Console.WriteLine($"Hello, {userName}, today is {currenDate.ToShortDateString()}.");
+            Console.ReadLine();
+        }
+    }
+}
