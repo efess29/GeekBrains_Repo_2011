db93063234dedb04d2902844bbe31ba37c66a974
efess29 <efess29@gmail.com>
2022-01-25 17:25:10 +0300

    fix task 1, add task 2

--- Lesson-2/Lesson-2_Task1/Program.cs
+++ Lesson-2/Lesson-2_Task1/Program.cs
@@ -7,53 +7,31 @@ namespace Lesson_2_Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose an option: 1 - calculate avg with array and loop, 2 - calculate avg with LYNQ-expression.");
-            string type = Convert.ToString(Console.ReadLine());
+            // Запрос минимальной и максимальной температуры (в decimal) за сутки и вывод среднего значения.
 
             Console.WriteLine("Enter min day temperature:");
-            decimal minTemp = decimal.Parse(Console.ReadLine());
+            string minInput = Console.ReadLine();
+
+            if (!decimal.TryParse(minInput, out decimal minTemp))
+            {
+                Console.WriteLine("Exception information: {0}", "Invalid min temperature value.");
+                return;
+            }
 
             Console.WriteLine("Enter max day temperature:");
-            decimal maxTemp = decimal.Parse(Console.ReadLine());
+            string maxInput = Console.ReadLine();
+
+            if (!decimal.TryParse(maxInput, out decimal maxTemp))
+            {
+                Console.WriteLine("Exception information: {0}", "Invalid max temperature value.");
+                return;
+            }
 
             decimal[] tempsArray = new decimal[] { minTemp, maxTemp };
 
-            switch (type)
-            {
-                // Подсчет сред. значения через массив и цикл
-                case "1":
-                {
-                    decimal summ = 0;
-                    
-                    for (int i = 0; i < tempsArray.Length; i++)
-                        summ += tempsArray[i];
-                    decimal avg = summ / tempsArray.Length;
-                    
-                    Console.WriteLine($"Average day temperature: {avg}.");
-                    Console.ReadLine();
-                    
-                    break;
-                }
-
-                // Подсчет сред. значения через lynq-выражение
-                case "2":
-                {
-                    try
-                    {
-                    decimal average = tempsArray.Average();
-                    Console.WriteLine($"Average day temperature: {average}.");
-                    }
-                
-                    catch
-                    {
-                        Console.WriteLine("One of the temperatures is null!");
-                    }
-                
-                    Console.ReadLine();
-                
-                    break;
-                }
-            }
+            decimal average = tempsArray.Average();
+            Console.WriteLine($"Average day temperature: {average}.");
+            Console.ReadLine();
         }
     }
 }
