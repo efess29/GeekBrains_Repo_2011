9b22c86b468f2f2a0938c31d646148689705cdb5
efess29 <efess29@gmail.com>
2022-02-07 18:10:53 +0300

    add task 1

---
+++ Lesson-4/Lesson-4_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson_4_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("\t");
+
+            Console.WriteLine("Enter user's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("\t");
+
+            Console.WriteLine("Enter user's patronymic name: ");
+            string patronymic = Console.ReadLine();
+            Console.WriteLine("\t");
+
+            string fullName = GetFullName(firstName, lastName, patronymic);
+
+            Console.WriteLine($"Entered full name is: {fullName}.");
+            Console.ReadLine();
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return lastName + " " + firstName + " " + patronymic;
+        }
+    }
+}
