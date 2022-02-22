a6d2e7aa4d50436bd2fdf617a346f6e2f6e486bd
efess29 <efess29@gmail.com>
2022-02-22 11:30:13 +0300

    add task 1

---
+++ Lesson-7/Lesson-7_Task1/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Lesson-6_Task1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Lesson-6_Task1")]
+[assembly: AssemblyTitle("Lesson-6_Task1")]
+[assembly: AssemblyVersion("1.0.0.0")]
